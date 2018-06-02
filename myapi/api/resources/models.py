de95eacea2a2d89cacf8af5d5b938596cdac4d48
eyansky <ianeyansky@gmail.com>
2018-06-02 19:21:00 +0300

    [Feature #157937907] Testing Login endpoint

--- myapi/api/resources/models.py
+++ myapi/api/resources/models.py
@@ -1,22 +1,35 @@
-users = []
+users = [
+    {
+        "firstname": "wairimu",
+        "lastname": "wairimu",
+        "email": "wairimu@wairimu.com",
+        "username": "wairimu",
+        "password": "wairimu"
+    }
+]
+
 
 def add_user(data):
     users.append(data)
 
+
 def view_users():
     return users
 
+
 def view_username():
-  for i in users:
-    return i['username']
+    for i in users:
+        return i['username']
+
 
 def view_password():
-  for i in users:
-    return i['password']
+    for i in users:
+        return i['password']
 
-def login(username,password):
-  for i in users:
-    if i['username'] == username and i['password'] == password:
-      return True
-  else:
-    return False 
+
+def login(username, password):
+    for i in users:
+        if i['username'] == username and i['password'] == password:
+            return True
+    else:
+        return False
