4a46d6d701154b718971be3327d814e689e18c51
eyansky <ianeyansky@gmail.com>
2018-06-03 13:54:21 +0300

    [Feature #157939106] Adding the requests route

--- myapi/api/__init__.py
+++ myapi/api/__init__.py
@@ -5,6 +5,7 @@ from flask_jwt_extended import (
     get_jwt_identity
 )
 from myapi.api.endpoints.users import User_Register, User_Login
+from myapi.api.endpoints.requests import Create_Request
 
 app = Flask(__name__)
 api = Api(app)
@@ -14,3 +15,4 @@ jwt = JWTManager(app)
 
 api.add_resource(User_Register, '/api/v1/users/register')
 api.add_resource(User_Login, '/api/v1/users/login')
+api.add_resource(Create_Request, '/api/v1/users/requests')
