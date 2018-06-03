c42a4a139249d3c93430d3822920fee7a12d01fb
eyansky <ianeyansky@gmail.com>
2018-06-03 14:07:30 +0300

    [Feature #157939106] create Request endpoint

---
+++ myapi/api/endpoints/requests.py
@@ -0,0 +1,41 @@
+from flask_restful import Resource
+from flask_restful import reqparse
+from myapi.api.resources.serializers import request_serializer
+from flask_jwt_extended import (
+    JWTManager, jwt_required, create_access_token,
+    get_jwt_identity
+)
+
+from myapi.api.resources.models import add_request
+
+
+class Create_Request(Resource):
+    """
+    Create a request
+    """
+    def post(self):
+        """ Add a request """
+        parser = reqparse.RequestParser()
+        parser.add_argument("title",
+                            required=True,
+                            help="Enter Title")
+        parser.add_argument(
+            "request",
+            required=True,
+            help="Enter Request details...")
+
+        args = parser.parse_args()
+
+        title, request = args["title"], args["request"]
+        data = {
+            "title": title,
+            "request": request
+        }
+        # Add to database
+        add_request(data)
+
+        response = {
+            "status": "ok",
+            "message": "Request has been added"
+        }
+        return (response), 201
