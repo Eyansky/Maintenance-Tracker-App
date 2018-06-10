8b3e3943dee214675fc1008b32093663643bc4fb
eyansky <ianeyansky@gmail.com>
2018-06-10 12:44:07 +0300

    [Feature #158128015] user can create requests

--- myapi/api/endpoints/requests.py
+++ myapi/api/endpoints/requests.py
@@ -8,6 +8,43 @@ from myapi.api.database.models import add_request, allrequests
 
 
 class Request(Resource):
+         
+    def is_valid(item):
+        """ 
+        checking for valid credentials
+        """
+        errors = {}
+        if not item.get("title"):
+            errors["title"] = "title Details are required."
+
+        if not item.get("request"):
+            errors["request"] = "Reuest Details are required."
+
+        return len(errors) == 0, errors
+
+    @jwt_required
+    def post(self):
+        if request.is_json:
+            valid, errors = self.is_valid(request.json)
+            if not valid:
+                return {"status": "error", "data": errors}, 400
+            result = request.json
+            
+            title=result['title']
+            request=result['request']
+            username=get_jwt_identity()
+            status = "Pending"
+
+            request = {
+                "username": username,
+                "title": title,
+                "request": request,
+                "status":status}
+            
+
+            return {"status": "success", "data": "request has been created"}, 201
+        else:
+            return {"message": "Request should be in JSON", "status": "error"}, 400
 
     # all requests  
     @jwt_required
@@ -15,7 +52,6 @@ class Request(Resource):
         username = get_jwt_identity()
         result = allrequests(username)
         return (result), 200
-
 
     # single request
     @jwt_required
@@ -28,4 +64,3 @@ class Request(Resource):
                 "message": "Reqeust not found"
             }, 404
         return (request), 200 
-  
